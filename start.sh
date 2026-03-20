0b55810df90abd3490a2e137dbc5922663c85845
batou <58328740+methatronc@users.noreply.github.com>
2026-03-20 13:45:13 +0100

    fix: added the venv to the python scripts

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-git pull
-
-source pyvenv/bin/activate
-
 firefox --kiosk dashboard.html &
 
 screen -S sensor
+screen -r sensor -p 0 -X stuff "source pyvenv/bin/activate $(printf '\r')"
 screen -r sensor -p 0 -X stuff "python read_sensor.py $(printf '\r')"
 
 screen -S app
+screen -r app -p 0 -X stuff "source pyvenv/bin/activate $(printf '\r')"
 screen -r app -p 0 -X stuff "python app.py $(printf '\r')"
