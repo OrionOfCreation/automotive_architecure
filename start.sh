cf0353d6233342be1eaeb77eb41e6ec2d26cfa8e
batou <58328740+methatronc@users.noreply.github.com>
2026-03-20 13:42:52 +0100

    build: added the screen logic to the startup process

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
 git pull
+
 source pyvenv/bin/activate
+
 firefox --kiosk dashboard.html &
-python read_sensor.py &
-python app.py
+
+screen -S sensor
+screen -r sensor -p 0 -X stuff "python read_sensor.py $(printf '\r')"
+
+screen -S app
+screen -r app -p 0 -X stuff "python app.py $(printf '\r')"
