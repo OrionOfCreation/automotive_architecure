835dd824e2c1fcf43706ebf4356bb8ce134578be
batou <58328740+methatronc@users.noreply.github.com>
2026-03-20 15:11:34 +0100

    fix: kills old screens first

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 firefox --kiosk dashboard.html &
 
-for component in can app; do
+for component in app can; do
+    screen -X -S $component quit
     screen -Sdm $component
     screen -r $component -p 0 -X stuff "source pyvenv/bin/activate $(printf '\r')"
     screen -r $component -p 0 -X stuff "python $component.py $(printf '\r')"
