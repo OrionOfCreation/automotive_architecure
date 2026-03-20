002594321c249e09ad52d82d24ad694de59863d5
batou <58328740+methatronc@users.noreply.github.com>
2026-03-20 13:53:33 +0100

    fix: starts the screens sessions in detached mode

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 firefox --kiosk dashboard.html &
 
-screen -S sensor
+screen -Sdm sensor
 screen -r sensor -p 0 -X stuff "source pyvenv/bin/activate $(printf '\r')"
 screen -r sensor -p 0 -X stuff "python read_sensor.py $(printf '\r')"
 
-screen -S app
+screen -Sdm app
 screen -r app -p 0 -X stuff "source pyvenv/bin/activate $(printf '\r')"
 screen -r app -p 0 -X stuff "python app.py $(printf '\r')"
